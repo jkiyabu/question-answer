a7d7b2ef3ece2fc599e80a1b5f98e3fad84e8baa
Jayeson Kiyabu <jkiyabu@comast.net>
2017-03-23 21:55:19 -0700

    add ability to edit a question

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('deleteQuestion', question);
       }
+    },
+    update(question, params) {
+      this.sendAction('update', question, params);
     }
   }
 });
--- app/components/question-update.js
+++ app/components/question-update.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestion: false,
+  actions: {
+    updateFormShow() {
+      this.set('updateFormShow', true);
+    },
+    update(question) {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes')
+      };
+      this.set('updateFormShow', false);
+      this.sendAction('update', question, params);
+    }
+  }
 });
